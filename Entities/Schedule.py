aa39a79832cbba3fb2a76823d6f4c298b3d79a43
Ahmad Khalid Karimzai <ak.karimzai1@mail.ru>
2022-12-07 22:22:50 +0300

    deleted

--- Entities/Schedule.py
+++
@@ -1,6 +0,0 @@
-class Schedule:
-    def __init__(self, id=None, excursionId='', day='', time=''):
-        self.id = id
-        self.excursionId = excursionId
-        self.day = day
-        self.time = time
