0fd55bccfb2da0d9d131d40a4d524111b6427578
Ahmad Khalid Karimzai <ak.karimzai1@mail.ru>
2022-12-23 22:18:21 +0300

    allure dependice added

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 python3 -m pytest Tests/UnitTests/GuideServiceUnitTest.py --alluredir=allure-results
 python3 -m pytest Tests/UnitTests/GuideUnitTest.py --alluredir=allure-results
+allure generate allure-results
+allure serve allure-results
